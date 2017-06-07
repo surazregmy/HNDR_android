26e5102334b82173415f37c5f9f5cb2ec4373406
suraz <surazz.regmi123@gmail.com>
2017-06-07 23:10:45 +0545

    Camera Access Provided

--- app/src/main/java/com/example/suraz/testopencv/MainActivity.java
+++ app/src/main/java/com/example/suraz/testopencv/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.suraz.testopencv;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.widget.TextView;
 
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.*;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -14,28 +20,107 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("opencv_java3");
     }
 
+    Mat mRgba;
+    private static String TAG  = "MainActivity";
+    JavaCameraView javaCameraView;
+    BaseLoaderCallback mLoaderCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status){
+                case  BaseLoaderCallback.SUCCESS:{
+                    javaCameraView.enableView();
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+            super.onManagerConnected(status);
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+//
+//        if(OpenCVLoader.initDebug()){
+//            tv.setText(tv.getText()+"\n OpenCV is KK working Congrats \n"+ "The " +
+//                    "version is "+ Core.VERSION);
+//
+//
+//        }
+//        else {
+//            tv.setText(tv.getText()+"\n OpenCv is not working");
+////            tv.setText(tv.getText() + "\n" + validate(0L,0L));
+//        }
+        //THIS ONE CALLS THE CAMERA
+        javaCameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+    }
 
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        if (javaCameraView != null)
+            javaCameraView.disableView();;
+
+    }
+
+    @Override
+    protected  void onDestroy(){
+        super.onDestroy();
+        if (javaCameraView != null)
+            javaCameraView.disableView();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
         if(OpenCVLoader.initDebug()){
-            tv.setText(tv.getText()+"\n OpenCV is working Congrats");
+            Log.i(TAG,"Hi this is wordkin");
+            mLoaderCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+
+
         }
         else {
-            tv.setText(tv.getText()+"\n OpenCv is not working");
-//            tv.setText(tv.getText() + "\n" + validate(0L,0L));
+            Log.i(TAG,"Hi this is not working");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallBack);
+//
         }
+
     }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        return mRgba;
+    }
 //    public native String validate(long matAddGr, long matAddrRgba);
 }
